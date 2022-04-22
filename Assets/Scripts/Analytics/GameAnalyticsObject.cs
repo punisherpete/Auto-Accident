5ec251198c2be852993d555c733f9d44a14ffe22
Konev_Daniil <konev.danKo@yandex.ru>
2022-04-22 13:55:34 +0300

    new wheel

--- Assets/Scripts/Analytics/GameAnalyticsObject.cs
+++ Assets/Scripts/Analytics/GameAnalyticsObject.cs
@@ -47,4 +47,20 @@ public class GameAnalyticsObject : ScriptableObject
             {"level",levelNumber }
         });
     }
+
+    public void OnGameExit(string registrationDate, int sessionCount, int daysInGame)
+    {
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Undefined, "reg_day", new Dictionary<string, object>()
+        {
+            {"date", registrationDate }
+        });
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Undefined, "session_count", new Dictionary<string, object>()
+        {
+            {"count", sessionCount }
+        });
+        GameAnalytics.NewProgressionEvent(GAProgressionStatus.Undefined, "days_in_game", new Dictionary<string, object>()
+        {
+            {"day", daysInGame }
+        });
+    }
 }
