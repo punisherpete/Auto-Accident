eba02e7407d19f344c003739ca1d27753ca01fe1
Valentin Kravchenko <59805441+Rowingist@users.noreply.github.com>
2022-04-26 23:38:21 +0300

    Reconverted terrain Camera and particles optimization

--- Assets/Scripts/Effects/CameraHeightZooming.cs
+++ Assets/Scripts/Effects/CameraHeightZooming.cs
@@ -7,21 +7,28 @@ public class CameraHeightZooming : MonoBehaviour
 {
     [SerializeField] private Car _car;
     [SerializeField] private float _moveSpeed = 0.05f;
+    [SerializeField] private float _allowOffsetWalue = 1;
 
-    private float _startCarY;
+    private float _currentGroundSurfacePoint;
+    private float _previousGroundSurfacePoint;
     private SplineProjector _projector;
 
     private void Awake()
     {
         _projector = GetComponent<SplineProjector>();
+        _previousGroundSurfacePoint = _currentGroundSurfacePoint;
     }
 
     private void LateUpdate()
     {
         if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit))
-            _startCarY = hit.point.y;
+            _currentGroundSurfacePoint = hit.point.y;
 
-        _projector.motion.offset = Vector2.Lerp(_projector.motion.offset, 
-            new Vector2(0, _car.transform.position.y - _startCarY), _moveSpeed * Time.deltaTime);
+        if (_currentGroundSurfacePoint - _previousGroundSurfacePoint >= _allowOffsetWalue)
+        {
+            _projector.motion.offset = Vector2.Lerp(_projector.motion.offset,
+                new Vector2(0, _car.transform.position.y - _currentGroundSurfacePoint), _moveSpeed * Time.deltaTime);
+            _previousGroundSurfacePoint = _currentGroundSurfacePoint;
+        }
     }
 }
--- Assets/Scripts/Effects/EffectsGenerator.cs
+++ Assets/Scripts/Effects/EffectsGenerator.cs
@@ -1,15 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EffectsGenerator : MonoBehaviour
 {
     [SerializeField] private ParticleSystem[] _effects;
 
-    public void Play(Vector3 spawnPosition)
+    public void Play(Vector3 position)
     {
         for (int i = 0; i < _effects.Length; i++)
         {
-            ParticleSystem generatedEffect = Instantiate(_effects[i], spawnPosition, Quaternion.identity, transform);
-            Destroy(generatedEffect.gameObject, generatedEffect.main.duration + generatedEffect.main.duration);
+            _effects[i].transform.position = position;
+            _effects[i].Play();
         }
     }
 }
