c300591b35b41432224193e4d253cda41bfc2e1a
Valentin Kravchenko <59805441+Rowingist@users.noreply.github.com>
2022-04-28 22:36:36 +0300

    Effects fix

--- Assets/Scripts/Effects/EffectsGenerator.cs
+++ Assets/Scripts/Effects/EffectsGenerator.cs
@@ -5,6 +5,11 @@ public class EffectsGenerator : MonoBehaviour
 {
     [SerializeField] private ParticleSystem[] _effects;
 
+    private void Awake()
+    {
+        Play(-transform.up * 100);
+    }
+
     public void Play(Vector3 position)
     {
         for (int i = 0; i < _effects.Length; i++)
