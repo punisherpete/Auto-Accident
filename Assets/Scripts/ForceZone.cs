8f106984ff6b49305fc1dbdd729cafe049456847
Valentin Kravchenko <59805441+Rowingist@users.noreply.github.com>
2022-05-23 23:21:21 +0300

    ForceZones prefabs

---
+++ Assets/Scripts/ForceZone.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class ForceZone : MonoBehaviour
+{
+    [SerializeField] private float _force;
+    [SerializeField] private ImpactDirection _impactDirection;
+
+    private Vector3 _impact;
+
+    private void Start()
+    {
+        SetImpactVector();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out Rigidbody rigidbody))
+        {
+            if (rigidbody.GetComponent<Mover>())
+            {
+                rigidbody.AddForce(_impact * _force, ForceMode.VelocityChange);
+            }
+        }
+    }
+
+    private void SetImpactVector()
+    {
+        switch (_impactDirection)
+        {
+            case ImpactDirection.Up:
+                _impact = Vector3.up;
+                break;
+            case ImpactDirection.Right:
+                _impact = Vector3.right;
+                break;
+            case ImpactDirection.Left:
+                _impact = Vector3.left;
+                break;
+            case ImpactDirection.Down:
+                _impact = Vector3.down;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public enum ImpactDirection
+{
+    Up,
+    Right,
+    Left,
+    Down
+}
