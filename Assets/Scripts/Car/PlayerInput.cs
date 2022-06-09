e8d89d2bb3f5dc02d841c2af951871561f566b68
Valentin Kravchenko <59805441+Rowingist@users.noreply.github.com>
2022-06-09 13:40:23 +0300

    Input

--- Assets/Scripts/Car/PlayerInput.cs
+++ Assets/Scripts/Car/PlayerInput.cs
@@ -24,7 +24,7 @@ public class PlayerInput : MonoBehaviour
 
     private void Update()
     {
-        if (_joystick.IsPointerDown)
+        if (_joystick.IsPointerDown && _mover.IsOnGround)
         {
             if (_isAbleToTurnInstanly)
             {
