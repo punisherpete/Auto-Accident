8cd62955fe70ffd8eab64f372a6d0b9d6de2ea8d
TylenbM19 <e-e-dsf-dsf@yandex.ru>
2022-11-16 19:37:21 +0300

    ls

--- Assets/Scripts/Data/ScreensaverSceneManager.cs
+++ Assets/Scripts/Data/ScreensaverSceneManager.cs
@@ -6,14 +6,14 @@ public class ScreensaverSceneManager : MonoBehaviour
 {
     [SerializeField] private bool _isRemoveDataOnStart = false;
     [SerializeField] private Data _data;
-    [SerializeField] private GameObject _musicPlayer;
+    //[SerializeField] private GameObject _musicPlayer;
 
     private void Start()
     {
         if (_isRemoveDataOnStart)
             _data.RemoveData();
-
-        DontDestroyOnLoad(_musicPlayer);
+        
+        // DontDestroyOnLoad(_musicPlayer);
 
         CheckSaveFile();
         _data.AddSession();
