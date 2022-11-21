6b4730e4ea1485af34547b63de6296c3f4c72f27
TylenbM19 <e-e-dsf-dsf@yandex.ru>
2022-11-21 20:45:00 +0300

    fix localized. add button close. Optimized.

--- Assets/Scripts/UI/CloseOnTap.cs
+++ Assets/Scripts/UI/CloseOnTap.cs
@@ -1,6 +1,6 @@
 using UnityEngine.EventSystems;
 using UnityEngine;
-using System.Collections.Generic;
+
 
 public class CloseOnTap : MonoBehaviour
 {
@@ -11,23 +11,23 @@ public class CloseOnTap : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(gameObject);
     }
 
-    private void Update()
-    {
-        PointerEventData pointer = new PointerEventData(EventSystem.current);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            pointer.position = Input.mousePosition;
-
-            List<RaycastResult> raycastResults = new List<RaycastResult>();
-            EventSystem.current.RaycastAll(pointer, raycastResults);
-
-            if (raycastResults.Count == 0)
-            {
-                CloseSelf();
-            }
-        }
-    }
+    // private void Update()
+    // {
+    //     PointerEventData pointer = new PointerEventData(EventSystem.current);
+    //
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         pointer.position = Input.mousePosition;
+    //
+    //         List<RaycastResult> raycastResults = new List<RaycastResult>();
+    //         EventSystem.current.RaycastAll(pointer, raycastResults);
+    //
+    //         if (raycastResults.Count == 0)
+    //         {
+    //             CloseSelf();
+    //         }
+    //     }
+    // }
 
     public void CloseSelf()
     {
